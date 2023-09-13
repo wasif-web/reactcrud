c6c641be9670f11f2bbad18788cf2ae625c32e98
Wasif Raza <63885026+wasif-web@users.noreply.github.com>
2023-09-13 21:30:16 +0500

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-const baseUrl = 'http://localhost:5001';
+const baseUrl = 'https://reactcrudbackhend.uc.r.appspot.com';
 
 function App() {
   const titleInputRef = useRef(null);
